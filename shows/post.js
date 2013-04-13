e33641576e9ee53a6e08082774086c0b748b42a0
yyolk <input.output@gmail.com>
2013-04-12 19:53:39 -0500

    whoops

--- shows/post.js
+++ shows/post.js
@@ -3,7 +3,7 @@ function(doc, req) {
     "Access-Control-Allow-Origin": "*", 
     "Content-Type:" : "text/html",
     "Charset" : "UTF-8"   },
-    body: '<html><head><meta property="og:title" content="NOTHINGBETWEEN.US"> <meta property="og:type" content="website"> <meta property="og:url" content="http://nothingbetween.us/p/'+doc._id+'"> <meta property="og:image" content="http://nothingbetween.us/nbu.png"> <meta property="og:description" content="'+doc.message+'"><link rel="stylesheet" href="/style/main.css" type="text/css"></link><script src="/script/analytics.js"></script></head><body onclick="location'+"'http://nothingbetween.us'"+';" style="cursor:pointer;'+doc.style+'"><div style="position:absolute;left:50%;top:50%;"><div style="position:relative;top:-50%;left:-50%;">'+doc.message+'</div></div></body></html>'
+    body: '<html><head><meta property="og:title" content="NOTHINGBETWEEN.US"> <meta property="og:type" content="website"> <meta property="og:url" content="http://nothingbetween.us/p/'+doc._id+'"> <meta property="og:image" content="http://nothingbetween.us/nbu.png"> <meta property="og:description" content="'+doc.message+'"><link rel="stylesheet" href="/style/main.css" type="text/css"></link><script src="/script/analytics.js"></script></head><body onclick="location.href='+"'http://nothingbetween.us'"+';" style="cursor:pointer;'+doc.style+'"><div style="position:absolute;left:50%;top:50%;"><div style="position:relative;top:-50%;left:-50%;">'+doc.message+'</div></div></body></html>'
      };
 }
 
